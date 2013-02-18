de53aa8aea2332709d03d5bf1b70033c49d1e06f
Steve Solomon <steven.solomon@snetltd.com>
2013-02-18 09:39:29 +0000

    Added Extension method for getting All
    Handy extension method on PageResults<T> for getting all results.

---
+++ Amido.Azure.Storage.TableStorage/Extensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Amido.Azure.Storage.TableStorage.Paging;
+using Microsoft.WindowsAzure.StorageClient;
+
+namespace Amido.Azure.Storage.TableStorage
+{
+    public static class Extensions
+    {
+        public static IList<TEntity> All<TEntity>(this PagedResults<TEntity> results) where TEntity : TableServiceEntity
+        {
+            if(results == null)
+            {
+                return null;
+            }
+
+            string continuationToken = null;
+            var entities = new List<TEntity>();
+            do 
+            {
+                var pagedResults = results;
+                continuationToken = pagedResults.ContinuationToken;
+                entities.AddRange(pagedResults.Results);
+
+            } while(continuationToken != null);
+
+            return entities;
+        }
+    }
+}
